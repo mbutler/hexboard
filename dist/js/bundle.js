ff66ead1b66b14286a384cca5ef85313c60a28f3
mbutler <matthewtbutler@gmail.com>
2019-10-29 23:54:23 -0500

    style the stage to fit screen

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -140,7 +140,7 @@ eval("var g;\n\n// This works in non-strict mode\ng = (function() {\n\treturn th
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n    value: true\n});\nvar map = exports.map = {\n    width: 100,\n    height: 500,\n    hexSize: 25,\n    divContainer: 'stage',\n    orientation: 'flat',\n    strokeWidth: 1,\n    strokeColor: '#D0D0D0'\n};\n\n//# sourceURL=webpack:///./src/config.js?");
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n    value: true\n});\nvar map = exports.map = {\n    width: 100,\n    height: 100,\n    hexSize: 25,\n    divContainer: 'stage',\n    orientation: 'flat',\n    strokeWidth: 1,\n    strokeColor: '#D0D0D0'\n};\n\n//# sourceURL=webpack:///./src/config.js?");
 
 /***/ }),
 
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const map = {
     width: 100,
-    height: 500,
+    height: 100,
     hexSize: 25,
     divContainer: 'stage',
     orientation: 'flat',
